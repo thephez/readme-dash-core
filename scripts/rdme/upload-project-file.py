8c9969737d647e083d720ff0d5d7151a1ccd3ed0
thephez <thephez@users.noreply.github.com>
2019-11-20 15:42:13 -0500

    script: get filenames as params
    add basic exception handling for large upload issue, small refactor

--- scripts/rdme/upload-project-file.py
+++ scripts/rdme/upload-project-file.py
@@ -2,6 +2,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 import os
+import sys
 
 API_KEY = os.environ.get("DASH_CORE_README_API_KEY")
 VERSION = 'v0.15.0'
@@ -13,50 +14,54 @@ def api_put(url, payload, headers=None):
     return response.json()
 
 def read_json_from_file(filename):
-    print('Reading from: {}/{}'.format(os.getcwd(), filename))
+    #print('Reading from: {}/{}'.format(os.getcwd(), filename))
     with open(filename, "r") as f:
         content = json.load(f)
 
     return content
 
 def read_from_file(filename):
-    print('Reading from: {}/{}'.format(os.getcwd(), filename))
+    #print('Reading from: {}/{}'.format(os.getcwd(), filename))
     with open(filename, "r") as f:
         content = f.readlines()
 
     return content
 
-
-def main():
-    filename = 'api-upload-test'
-
-    # Contains title, category, slug info
-    json_data = read_json_from_file(('full-json/{}.json'.format(filename))
-
-    # Text content
-    markdown_data = read_from_file('markdown/{}.md'.format(filename))
-    full_text = ''.join(markdown_data)
-    
+def create_payload(json_data, markdown_text):
     # Construct payload for Readme.io
     payload = {}
     payload['title'] = json_data['title']
     payload['type'] = json_data['type']
     payload['category'] = json_data['category']
-    payload['body'] = full_text
+    payload['body'] = markdown_text
 
-    slug = json_data['slug']
+    return payload
 
-    url = '{}/{}'.format("https://dash.readme.io/api/v1/docs", slug)
-    headers = {'x-readme-version': 'v0.15.0'}
-    
-    response = api_put(url, payload, headers)
-    print('Updated: "{}" ({})'.format(payload['title'], url))
-    #if response['body'] == payload['body']:
-    #    print('Successfully updated: "{}"'.format(payload['title']))
-    #else:
-        #print('Update failed for: "{}"'.format(payload['title']))
-        #print(response)
-        #print(full_text)
+
+def main(args):
+    for i in range(1, len(args)):
+        filename = args[i] #'api-upload-test'
+
+        # Contains title, category, slug info
+        json_data = read_json_from_file('full-json/{}.json'.format(filename))
+
+        # Text content
+        markdown_lines = read_from_file('markdown/{}.md'.format(filename))
+        full_text = ''.join(markdown_lines)
+
+        url = '{}/{}'.format("https://dash.readme.io/api/v1/docs", json_data['slug'])
+        headers = {'x-readme-version': VERSION}
+
+        # Create payload and send
+        payload = create_payload(json_data, full_text)
+        try:
+            response = api_put(url, payload, headers)
+            # TODO: add some checking to verify upload succesful
+            print('Updated: "{}" ({})'.format(payload['title'], url))
+        except requests.HTTPError as e:
+            print('--- Exception updating "{}": {} ---'.format(payload['title'], e))
+            continue
+
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
