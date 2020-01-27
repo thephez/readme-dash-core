1f2589cce755f9db96926e5b4295d806aa0297db
thephez <thephez@users.noreply.github.com>
2020-01-27 16:55:02 -0500

    script: push

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,21 +6,22 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b backup
+  git pull
+  git checkout -b backup-test001
   #git add . *.json
   git add -A
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/thephez/rdme-core.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages core-backup
+  git remote add origin-pages https://${GH_TOKEN}@github.com/thephez/readme-dash-core.git > /dev/null 2>&1
+  git push -f --set-upstream origin-pages backup-test0001
 }
 
 setup_git
 commit_website_files
 upload_files
 
-git remote -v
+#git remote -v
 #git status
 #git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
