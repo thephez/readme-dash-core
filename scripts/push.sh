470f934103309b708019102365f83603fda40c94
thephez <thephez@users.noreply.github.com>
2020-01-27 16:11:36 -0500

    build: update travis and add push script

---
+++ scripts/push.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b backup
+  git add . *.html
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/thephez/rdme-core.git > /dev/null 2>&1
+  #git push --quiet --set-upstream origin-pages core-backup
+}
+
+cd
+setup_git
+commit_website_files
+upload_files
+
+git remote -v
+git status
+git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
