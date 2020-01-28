02d24c835d2f798619e985592cf59e1db31d0569
thephez <thephez@users.noreply.github.com>
2020-01-28 11:52:35 -0500

    build: script cleanup

--- scripts/push.sh
+++ scripts/push.sh
@@ -9,7 +9,7 @@ setup_git() {
 
 prepare_backup_repo() {
   cd $TRAVIS_BUILD_DIR
-  git clone https://${GH_TOKEN}@github.com/thephez/rdme-core.git $BACKUP_REPO_DIR #> /dev/null 2>&1
+  git clone https://${GH_TOKEN}@github.com/thephez/backup-core-readme.git $BACKUP_REPO_DIR
   rm -rf $BACKUP_REPO_DIR/*
   mkdir -p $BACKUP_REPO_DIR/docs
   cp -R $TRAVIS_BUILD_DIR/export/* $BACKUP_REPO_DIR
@@ -17,15 +17,12 @@ prepare_backup_repo() {
 
 commit_website_files() {
   cd $BACKUP_REPO_DIR
-  #git pull --rebase
-  #git checkout -b backup-test001
   git add -A
   git commit --message "Travis auto-backup: $TRAVIS_BUILD_NUMBER at $DT"
 }
 
 upload_files() {
-  #git remote add origin-pages https://${GH_TOKEN}@github.com/thephez/readme-dash-core.git > /dev/null 2>&1
-  git push --set-upstream origin master #backup-test001
+  git push --set-upstream origin master
 }
 
 setup_git
