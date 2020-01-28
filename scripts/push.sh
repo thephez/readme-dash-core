edbfa703c0271261ac32c2f263767941ad83bb9c
thephez <thephez@users.noreply.github.com>
2020-01-28 08:40:14 -0500

    script: cleanup

--- scripts/push.sh
+++ scripts/push.sh
@@ -29,20 +29,5 @@ cd ..
 git clone https://${GH_TOKEN}@github.com/thephez/readme-dash-core.git rdme #> /dev/null 2>&1
 cp -R $TRAVIS_BUILD_DIR/export rdme
 
-#git push
-
-#mkdir -p ../rdme-core/
-#cp -R export/ ../rdme-core
-#ls ../rdme-core
-#ls ../rdme-core/export
-
-#cd ../rdme-core
-#git init
-#git status
-
 commit_website_files
 upload_files
-
-#git remote -v
-#git status
-#git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
