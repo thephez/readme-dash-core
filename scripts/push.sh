fefbf94a074012ff1e111d5a617720ff19f5f7c2
thephez <thephez@users.noreply.github.com>
2020-01-28 10:03:51 -0500

    script: cleanup

--- scripts/push.sh
+++ scripts/push.sh
@@ -9,27 +9,20 @@ commit_website_files() {
   cd rdme
   #git pull --rebase
   #git checkout -b backup-test001
-  #git add . *.json
   git add -A
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git commit --message "Travis auto-backup: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   #git remote add origin-pages https://${GH_TOKEN}@github.com/thephez/readme-dash-core.git > /dev/null 2>&1
   git push --set-upstream origin master #backup-test001
-  git status
-  git show-ref
-  git branch
 }
 
 setup_git
 
-#ls $TRAVIS_BUILD_DIR
 cd ..
 git clone https://${GH_TOKEN}@github.com/thephez/rdme-core.git rdme #> /dev/null 2>&1
-ls
 rm -rf rdme/*
-ls
 mkdir -p rdme/docs
 cp -R $TRAVIS_BUILD_DIR/export/* rdme/docs
 
